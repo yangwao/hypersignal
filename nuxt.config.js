1b27a70093b5765a1ff42a2f1dd5fd70f8715a81
Matej Nemcek <ybdaba@gmail.com>
2017-07-02 22:00:05 +0200

    back to base /

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
   ** Customize the progress-bar color
   */
   loading: { color: '#3B8070' },
-  router: { base: '/hypersignal/' },
+  router: { base: '/' },
   /*
   ** Build configuration
   */
